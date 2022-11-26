d0b38dd50427ef06df7c293bc34081119cad8e2c
i-lili <113575743+i-lili@users.noreply.github.com>
2022-11-26 13:42:05 +0300

    fix review

--- scripts/index.js
+++ scripts/index.js
@@ -7,15 +7,13 @@ const popupOpenButtonElement = document.querySelector(".profile__edit-button");
 
 const openPopup = function () {
   popupElement.classList.add("popup_is-opened");
-  console.log("Open popup clicked");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 };
 
 const closePopup = function () {
   popupElement.classList.remove("popup_is-opened");
 };
-
-popupOpenButtonElement.addEventListener("click", openPopup);
-popupCloseButtonElement.addEventListener("click", closePopup);
 
 // Редактированиe профиля
 
@@ -25,14 +23,14 @@ const jobInput = formElement.querySelector(".popup__input_item_job");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
 
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
+
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closePopup();
 }
 
+popupOpenButtonElement.addEventListener("click", openPopup);
+popupCloseButtonElement.addEventListener("click", closePopup);
 formElement.addEventListener("submit", formSubmitHandler);
