90d236665e2e5ee452b20801178dce44c2ec9970
i-lili <113575743+i-lili@users.noreply.github.com>
2023-02-25 16:51:11 +0300

    refactoring, building with webpack

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 // подключаем плагины в файл
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
 
 module.exports = {
   // подключаем плагины к PostCSS
@@ -10,6 +10,6 @@ module.exports = {
     // cssnano при подключении передаем объект опций
     // { preset: default } говорит о том, что нужно использовать
     // стандартные настройки минификации
-    cssnano({ preset: 'default' })
-  ]
+    cssnano({ preset: "default" }),
+  ],
 };
